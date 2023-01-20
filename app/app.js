084cc872ecca13940310bb544b835fc4b6f21f25
crypticalfish86 <jasonweerawardena@gmail.com>
2023-01-20 02:42:57 +0000

    this branch exists

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const {getTopics, getArticles, getArticleById, getComments} = require('./controller.js')
+app.use(express.json())
 
 app.get('/api/topics', getTopics);
 
@@ -9,6 +10,25 @@ app.get('/api/articles', getArticles);
 app.get('/api/articles/:article_id', getArticleById)
 
 app.get('/api/articles/:article_id/comments', getComments)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.delete('/api/comments/:comment_id')
 
 
 app.use((error, request, response, next) =>
