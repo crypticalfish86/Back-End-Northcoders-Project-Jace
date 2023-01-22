4b229e3afffd58b0f7ef69ba07693ea812143f69
crypticalfish86 <jasonweerawardena@gmail.com>
2023-01-22 22:25:51 +0000

    merge issues resolved

--- app/app.js
+++ app/app.js
@@ -15,13 +15,22 @@ app.get('/api/articles/:article_id', getArticleById)
 
 app.get('/api/articles/:article_id/comments', getComments)
 
+app.post('/api/articles/:article_id/comments', postUserComment)
+
 app.patch('/api/articles/:article_id', patchArticleVotes)
 
 app.get('/api/users', getUsers)
 
 
 
-
+app.use((error, request, response, next) =>
+{
+    if(error.status && error.msg)
+    {
+        const {status, msg} = error
+        response.status(status).send({msg: msg})
+    }
+})
 module.exports = app;
 
 
@@ -104,13 +113,4 @@ module.exports = app;
 
 
 
-app.post('/api/articles/:article_id/comments', postUserComment)
 
-app.use((error, request, response, next) =>
-{
-    if(error.status && error.msg)
-    {
-        const {status, msg} = error
-        response.status(status).send({msg: msg})
-    }
-})
