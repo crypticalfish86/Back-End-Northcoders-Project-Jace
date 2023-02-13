935fd8f8e132b5c25f03a837cadb0a194fa4a65c
Jason <jasonweerawardena@gmail.com>
2023-02-13 21:35:37 +0000

    Delete Comment Refactor Complete

--- app/model.js
+++ app/model.js
@@ -301,5 +301,31 @@ const fetchUsers = () =>
     })
 }
 
+const removeComment = (comment_id) =>
+{
+    if(!/[0-9]+/g.test(comment_id))
+    {
+        return Promise.reject({status: 400, msg: 'invalid comment ID: not a number'})
+    }
+    return db.query
+    (
+        `
+        DELETE FROM comments
+        WHERE comment_id = ${comment_id}
+        RETURNING *
+        `
+    )
+    .then((response) =>
+    {
+        if(response.rowCount === 0)
+        {
+            return Promise.reject({status: 404, msg: 'invalid comment ID: ID not found'})
+        }
+        else
+        {
+            return response.rows
+        }
+    })
+}
 
 module.exports = {fetchTopics, fetchArticles, fetchArticleById, fetchComments,addUserComment, changeArticleVotes, fetchUsers, removeComment}
